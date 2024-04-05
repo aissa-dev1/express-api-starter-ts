6897d180b08cd9e9a29dc77d78b8b03c55b4caba
aissa-dev-1 <aissabedr74@gmail.com>
2024-04-05 07:02:47 +0100

    Delete the main controller

--- src/controller.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from "express";
-import MessageResponse from "./interfaces/MessageResponse";
-
-export function sayHello(req: Request, res: Response<MessageResponse>) {
-  return res.status(200).json({ message: "Hello world!" });
-}
