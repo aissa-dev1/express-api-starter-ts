692f8d09e760cc0b6b6e5bd315e5fc4cc866e518
aissa-dev-1 <aissabedr74@gmail.com>
2024-04-05 07:03:13 +0100

    Update main.ts file

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,30 @@
 import express, { json } from "express";
 import PORT from "./config/port";
-import { listen } from "./app";
 import cors from "cors";
 import apiRouter from "./api";
-import * as controller from "./controller";
-
+import mongoose from "mongoose";
+import URI from "./config/uri";
 const app = express();
 
 app.use(json());
-
 app.use(cors());
+
+async function connect() {
+  try {
+    await mongoose.connect(URI);
+    console.log("DB connected");
+  } catch (error: any) {
+    console.log(error.message);
+  }
+}
+connect();
 
 app.use("/api", apiRouter);
 
-app.get("/", controller.sayHello);
+app.get("/", (req, res) => {
+  return res.status(200).json({ message: "Hello world!" });
+});
 
-app.listen(PORT, listen);
+app.listen(PORT, () => {
+  console.log(`App starts at: http://localhost:${PORT}`);
+});
