25270b3e9fcfaf2c0fa698fc2a861c920a39da47
aissa-dev-1 <aissabedr74@gmail.com>
2024-04-05 07:02:18 +0100

    Update the api

--- src/api/controller.ts
+++ src/api/controller.ts
@@ -1,11 +1,9 @@
 import { Request, Response } from "express";
-import MessageResponse from "../interfaces/MessageResponse";
-import EmojiResponse from "./types/EmojiResponse";
 
-export function getApi(req: Request, res: Response<MessageResponse>) {
-  return res.status(200).json({ message: "API - Aissa Bedr" });
+export function getApi(req: Request, res: Response) {
+  return res.status(200).json({ message: "API - aissa-dev1" });
 }
 
-export function getEmojis(req: Request, res: Response<EmojiResponse>) {
+export function getEmojis(req: Request, res: Response) {
   return res.status(200).json(["😐", "😉", "😎", "🤣"]);
 }
--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,8 @@
 import { Router } from "express";
 import emojisRouter from "./emojis";
 import * as controller from "./controller";
-import { connect } from "../app";
 
 const router = Router();
-
-connect();
 
 router.use("/emojis", emojisRouter);
 
